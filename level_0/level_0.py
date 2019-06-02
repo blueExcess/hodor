10d8ba4d103a2f34d517fea6c4c349630e6bf1b3
Nate Petersen <nate.petersen@me.com>
2019-06-02 02:16:20 +0000

    fix bug and make executable

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -2,7 +2,7 @@
 import mechanize
 
 br = mechanize.Browser()
-br.open(http://158.69.76.135/level0.php)
+br.open('http://158.69.76.135/level0.php')
 for i in range(1024):
     br.select_form(nr=0)
     br.form['id'] = '677'
