5dc443d2ad24169d1bdec376019df43cfab13223
Nate Petersen <nate.petersen@me.com>
2019-06-02 01:26:48 +0000

    fix ID to correct one

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -5,5 +5,5 @@ br = mechanize.Browser()
 br.open(http://158.69.76.135/level0.php)
 for i in range(1024):
     br.select_form(nr=0)
-    br.form['id'] = '667'
+    br.form['id'] = '677'
     br.submit()
